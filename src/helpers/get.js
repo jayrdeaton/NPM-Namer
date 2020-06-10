04e1171ce13ce73854aaf3c7122e579ef9d36a5a
jayrdeaton <jayrdeaton@gmail.com>
2020-06-10 17:52:31 -0500

    return just data

--- src/helpers/get.js
+++ src/helpers/get.js
@@ -1,4 +1,7 @@
 const axios = require('axios'),
   DOMAIN = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json'
 
-module.exports = (word) => axios.get(`${DOMAIN}/word?key=${process.env.MW_THESAURUS_KEY}`)
+module.exports = async (word) => {
+  const { data } = await axios.get(`${DOMAIN}/${word}?key=${process.env.MW_THESAURUS_KEY}`)
+  return data
+}
