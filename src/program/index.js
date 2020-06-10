f804447a3bd1db26a34be7e3f935e14a7b9b69a2
jayrdeaton <jayrdeaton@gmail.com>
2020-06-10 17:52:44 -0500

    name is a required variable

--- src/program/index.js
+++ src/program/index.js
@@ -3,7 +3,7 @@ const { command, option } = require('termkit'),
   { checkKey } = require('../middlewares'),
   { version } = require('../../package.json')
 
-const program = command('npm-namer', '[name]')
+const program = command('npm-namer', '<name>')
   .version(version)
   .description('A CLI utility for creating, checking, and removing lock files')
   .middleware(checkKey)
