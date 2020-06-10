efa43cefae00770ea43078aa0c233f17efc19f2e
jayrdeaton <jayrdeaton@gmail.com>
2020-06-10 17:49:38 -0500

    fix

--- src/helpers/get.js
+++ src/helpers/get.js
@@ -1,4 +1,4 @@
 const axios = require('axios'),
   DOMAIN = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json'
 
-module.exports = (word) => axios.get(`${domain}/word?key=${process.env.MW_THESAURUS_KEY}`)
+module.exports = (word) => axios.get(`${DOMAIN}/word?key=${process.env.MW_THESAURUS_KEY}`)
